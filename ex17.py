038229763e25ccfbf1e63005a7f8fcb9bfe616c5
GarborSerey <garborfersru@gmail.com>
2022-07-05 18:37:45 +0300

    __eq__, __hash__  ФУНКУИЯ hash и хэши объектов

---
+++ ex17.py
@@ -0,0 +1,23 @@
+# ==================== ФУНКУИЯ bool() и магические методы =======================
+
+# __len__() - вызывается функцией bool() если не определен магический метод __bool__
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        print('__len__')
+        return self.x * self.x + self.y * self.y
+
+    def __bool__(self):
+        print('__bool__')
+        return self.x == self.y
+
+
+
+p = Point(3, 3)
+print(len(p))
+print(bool(p))
